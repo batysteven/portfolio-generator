034c39f508fbb5d9f600d14a3a9f59fa6e25630a
Steven Baty <baty_steven@yahoo.com>
2021-01-29 22:14:01 -0700

    add notate of promise

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -20,6 +20,7 @@ const writeFile = fileContent => {
 // copying file
 const copyFile = () => {
   return new Promise((resolve, reject) => {
+    // taking style.css from src into dist
     fs.copyFile('./src/style.css', './dist/style.css', err => {
       if (err) {
         reject(err);
